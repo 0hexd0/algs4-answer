fc4626e19e34a037ffa59e93c67aa50b6ed0e5f7
HeXudong <1514133606@qq.com>
2023-12-10 13:04:36 +0800

    feat: ErdosRenyiRatio

---
+++ ErdosRenyiRatio.java
@@ -0,0 +1,46 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+
+public class ErdosRenyiRatio {
+    public static int count(int n) {
+        int edges = 0;
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while (uf.count() > 1) {
+            int i = StdRandom.uniformInt(0, n);
+            int j = StdRandom.uniformInt(0, n);
+            if (uf.find(i) != uf.find(j)) {
+                uf.union(i, j);
+            }
+            edges++;
+        }
+        return edges;
+    }
+
+    public static double timeTrial(int n, int trials) {
+        Stopwatch timer = new Stopwatch();
+        for (int i = 0; i < trials; i++) {
+            count(n);
+        }
+        return timer.elapsedTime();
+    }
+
+    public static void main(String[] args) {
+        int trials = Integer.parseInt(args[0]);
+
+        double prev = timeTrial(125, trials);
+        for (int n = 250; true; n += n) {
+            double time = timeTrial(n, trials);
+            StdOut.printf("%7d %7.1f %5.1f\n", n, time, time / prev);
+            prev = time;
+        }
+
+    }
+}
