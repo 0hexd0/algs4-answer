e9585f45195c7ab724ae113d857ea5ed7d37fc18
HeXudong <1514133606@qq.com>
2025-02-26 14:39:33 +0800

    feat: 2_3_29

---
+++ chapter3/section1/FrequencyCounter.java
@@ -0,0 +1,38 @@
+/* *****************************************************************************
+ *  Name:              He XuDong
+ **************************************************************************** */
+
+package chapter3.section1;
+
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class FrequencyCounter {
+    public static void main(String[] args) {
+        int minlen = Integer.parseInt(args[0]);
+        ST<String, Integer> st = new ST<String, Integer>();
+        String lastPutWord = "";
+        int wordCounter = 0;
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (word.length() < minlen) continue; // 忽略较短单词
+            if (!st.contains(word)) st.put(word, 1);
+            else {
+                st.put(word, st.get(word) + 1);
+            }
+            lastPutWord = word;
+            wordCounter++;
+        }
+        StdOut.println("The word counter is " + wordCounter);
+        StdOut.println("The last put word is " + lastPutWord);
+        String max = " ";
+        st.put(max, 0);
+        for (String word : st.keys()) {
+            if (st.get(word) > st.get(max)) {
+                max = word;
+            }
+        }
+        StdOut.println(max + " " + st.get(max));
+    }
+}
