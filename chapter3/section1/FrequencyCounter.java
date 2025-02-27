28588734a4750ca532cccb15fa6244a31ad34914
HeXudong <1514133606@qq.com>
2025-02-27 09:10:55 +0800

    feat: 2_3_29

--- chapter3/section1/FrequencyCounter.java
+++ chapter3/section1/FrequencyCounter.java
@@ -4,6 +4,7 @@
 
 package chapter3.section1;
 
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
@@ -26,13 +27,28 @@ public class FrequencyCounter {
         }
         StdOut.println("The word counter is " + wordCounter);
         StdOut.println("The last put word is " + lastPutWord);
-        String max = " ";
-        st.put(max, 0);
+        Queue<String> mostPutWordQ = new Queue<String>();
+        int lastMaxTimes = 0;
         for (String word : st.keys()) {
-            if (st.get(word) > st.get(max)) {
-                max = word;
+            int curTimes = st.get(word);
+            if (curTimes == lastMaxTimes) {
+                // 同最大一样，入队
+                mostPutWordQ.enqueue(word);
+            }
+            else if (curTimes > lastMaxTimes) {
+                // 超过最大，清空队列后入队
+                while (!mostPutWordQ.isEmpty()) {
+                    mostPutWordQ.dequeue();
+                }
+                mostPutWordQ.enqueue(word);
+                // 重设最大值
+                lastMaxTimes = curTimes;
             }
         }
-        StdOut.println(max + " " + st.get(max));
+        // 打印所有最大的key
+        StdOut.println("These words have most put times:");
+        for (String key : mostPutWordQ) {
+            StdOut.println(key + " " + st.get(key));
+        }
     }
 }
